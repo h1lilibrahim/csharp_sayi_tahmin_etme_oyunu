f9e16516cf2e7f43b3e254193436a2d0bde7a88f
Halil İbrahim Köklü <halilibrahimm3446@gmail.com>
2024-04-19 11:19:30 +0300

    sayı tahmin etme oyunu

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Random random = new Random();
+int rasgele = random.Next(0, 100);
+int başlangıç = 0;
+
+while (başlangıç != rasgele) 
+{
+    Console.Write("Rasgele sayı giriniz: ");
+    int sayı = Convert.ToInt32(Console.ReadLine());
+    if (sayı > rasgele)
+    {
+        Console.WriteLine("daha düşük bir sayı girin");
+        
+    }
+    else if (sayı < rasgele)
+    {
+        Console.WriteLine("daha büyük bir sayı girin");
+        
+    }
+    else if (sayı == rasgele)
+    {
+        break;
+    }
+}
+Console.WriteLine("doğru bildin");
